876acdece54898a233ae064f0ee5770dbbd01e1e
Jbarb25 <barboza205@usf.edu>
2025-02-06 15:10:54 -0500

    Task 3 - Employee Bonus Calculation
    Write an array function to calculate the bonus based on the performance rating. Calls the function and plugs in the salary and performance rating which finds the right percentage to apply to calculate the bonus and return it to the console.

--- cc_6.js
+++ cc_6.js
@@ -14,3 +14,14 @@ function calculateSalesTax(amount, taxRate){   //Declare a function that calcula
 //Call the function to plug in the amount and tax rate, then return the outcome to the console
 console.log(`Sales Tax: $${calculateSalesTax(100, 0.07)}`)
 console.log(`Sales Tax: $${calculateSalesTax(500, 0.1)}`)
+
+
+//Task 3: Arrow Function
+const calculateBonus = (salary, performanceRating) =>  //Write an arrow function to calculate the bonus with salary and performance rating parameters.
+    performanceRating === "Excellent" ? salary*0.2:  //is performance rating "excellent", yes then salary x 0.20 bonus
+    performanceRating === "Good" ? salary*0.1:  //is performance rating "good", yes then salary x 0.10 bonus
+    performanceRating === "Average" ? salary*0.05://is performance rating "average", yes then salary x 0.05 bonus
+    0;   //if none of these apply, no bonus
+
+console.log(`Bonus: $${calculateBonus(5000, "Excellent")}`)  //Call the arrow function and plug in the salary and performance rating
+console.log(`Bonus: $${calculateBonus(7000, "Good")}`)
