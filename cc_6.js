3f3cef3f56f5d32e1a9ff4d60dfc0d107f0705e3
Jbarb25 <barboza205@usf.edu>
2025-02-06 14:09:03 -0500

    Task 2 - Sales Tax Computation
    Declare a function to calculate sales tax then call the function to plug in the amount and tax rate that returns the outcome to the console.

--- cc_6.js
+++ cc_6.js
@@ -3,5 +3,14 @@ function calculateProfit(costPrice, sellingPrice, unitsSold){  //Write a functio
     [profit = (sellingPrice - costPrice)* unitsSold]
     return profit}
 //Calls the calculateProfit function, plugs the cost price, selling price, and units sold, and logs the outcome to console.
-console.log(`Total Profit: ${calculateProfit(20,30,100)}`)
-console.log(`Total Profit: ${calculateProfit(50,70,200)}`)
+console.log(`Total Profit: $${calculateProfit(20,30,100)}`)
+console.log(`Total Profit: $${calculateProfit(50,70,200)}`)
+
+
+//Task 2: Function Expression
+function calculateSalesTax(amount, taxRate){   //Declare a function that calculates sales tax
+    [salesTax = amount * taxRate]
+    return salesTax}
+//Call the function to plug in the amount and tax rate, then return the outcome to the console
+console.log(`Sales Tax: $${calculateSalesTax(100, 0.07)}`)
+console.log(`Sales Tax: $${calculateSalesTax(500, 0.1)}`)
